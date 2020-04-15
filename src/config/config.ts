3667d168e9063fc2f234cf1805a2e4990167b1b8
zzy <skunight@gmail.com>
2020-04-15 17:13:10 +0800

    add \n

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,7 +4,7 @@ export const config = {
   template: {
     github: '[github]\n\n'+
             'repository:<%= repository.full_name %>\n\n'+
-            '<%= pusher.name %> pushed on branch <%= ref %> with the following commit(s):\n'+
+            '<%= pusher.name %> pushed on branch <%= ref %> with the following commit(s):\n\n'+
             '<% _.forEach(commits, function(c) { %>[<%= c.id.substr(0, 7) %>] <%= c.message %>\n\n<% }); %>'
   }
 }
