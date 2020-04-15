99a2281453572d766d6ec86b4b5e06f1915dc9e3
zzy <skunight@gmail.com>
2020-04-15 17:04:13 +0800

    add template

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,10 @@
 export const config = {
   shortenUrlBase: 'https://s.bidwin.top/api/v1/shorten',
   wechatWorkHost: 'https://qyapi.weixin.qq.com',
+  template: {
+    github: '[github]\n\n'+
+            'repository:<%= repository.full_name %>\n\n'+
+            '<%= pusher.name %> pushed on branch <%= ref %> with the following commit(s):\n'+
+            '<% _.forEach(commits, function(c) { %>[<%= c.id.substr(0, 7) %>] <%= c.message %>\n\n<% }); %>'
+  }
 }
--- src/services/github.service.ts
+++ src/services/github.service.ts
@@ -1,19 +1,24 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Inject } from "@nestjs/common";
 import { HookParse, GithubData } from "../interfaces";
-
+import { template as _template } from 'lodash'
+import { CONFIG_PROIVDE } from "../config";
 @Injectable()
 export class GitHubHookService implements HookParse {
-  // parse(data: any, _to: string, _event?: string): string {
+  constructor(
+    @Inject(CONFIG_PROIVDE) private readonly config,
+  ) { }
+
   parse(data: GithubData): string {
     return this._push(data);
   }
 
   private _push(data: GithubData): string {
-    const message = `[github]
-      repository: ${data.repository.full_name}
-      ${data.pusher.name} pushed on branch ${data.ref} with the following commit(s):
-      ${data.commits.map((o: { id: string; message: string; }) => '[' + o.id.substr(0, 7) + '] ' + o.message).join("\n\n")}
-    `
-    return message.split(/\n/).map(o => o.trim()).join('\n')
+    // const message = `[github]
+    //   repository: ${data.repository.full_name}
+    //   ${data.pusher.name} pushed on branch ${data.ref} with the following commit(s):
+    //   ${data.commits.map((o: { id: string; message: string; }) => '[' + o.id.substr(0, 7) + '] ' + o.message).join("\n\n")}
+    // `
+    // return message.split(/\n/).map(o => o.trim()).join('\n')
+    return _template(this.config.template.github)(data)
   }
 }
