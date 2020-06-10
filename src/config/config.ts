15bf8ee34197dc2bd7670502e3a8a41c1d7e1b50
w.c <52445078@qq.com>
2020-06-10 17:46:01 +0800

    re-format

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,54 +4,54 @@ export const config = {
   template: {
     github: {
       issue: `
-\nissue标题: <%= title %>
-\nissue内容: <%= content %>
-\nissue地址: <%= url %>
-\n仓库: <%= repository %>
-\n操作: issue <%= action %>
-\n操作人: <%= actionLogin %>
+        issue标题: <%= title %>
+        issue内容: <%= content %>
+        issue地址: <%= url %>
+        仓库: <%= repository %>
+        操作: issue <%= action %>
+        操作人: <%= actionLogin %>
       `,
       issueComment: `
-\nissue标题: <%= title %>
-\nissue内容: <%= content %>
-\nissue地址: <%= url %>
-\nissueComment内容: <%= commentContent %>
-\nissueComment地址: <%= commentUrl %>
-\n仓库: <%= repository %>
-\n操作: issue comment <%= action %>
-\n操作人: <%= actionLogin %>
+        issue标题: <%= title %>
+        issue内容: <%= content %>
+        issue地址: <%= url %>
+        issueComment内容: <%= commentContent %>
+        issueComment地址: <%= commentUrl %>
+        仓库: <%= repository %>
+        操作: issue comment <%= action %>
+        操作人: <%= actionLogin %>
       `,
       create: `
-<%= actionLogin %> 在 <%= repository %>  创建了 <%= type %>: <%= name %>
-`,
+        <%= actionLogin %> 在 <%= repository %>  创建了 <%= type %>: <%= name %>
+      `,
       push: `
-\n分支: <%= name %>
-\n仓库: <%= repository %>
-\n是否新建: <%= created %>
-\n是否删除: <%= deleted %>
-\n是否强推: <%= forced %>
-\n分支比较页面: <%= compare %>
-\n本次提交: <%= commits %>
-\n操作人: <%= actionLogin %>      
+分支: <%= name %>
+仓库: <%= repository %>
+是否新建: <%= created %>
+是否删除: <%= deleted %>
+是否强推: <%= forced %>
+分支比较页面: <%= compare %>
+本次提交: <%= commits %>
+操作人: <%= actionLogin %>      
 `,
       pullRequest:`
-\nprAction: <%= action %>      
-\npr标题: <%= title %>      
-\npr地址: <%= url %>      
-\npr状态: <%= state %>
-\npr注释: <%= content %>
-\npr提交人: <%= actionLogin %>
-\npr提交时间: <%= createdAt %>
-\npr更新时间: <%= updatedAt %>
-\npr关闭时间: <%= closedAt %>
-\npr合并时间: <%= mergedAt %>
-\nprFrom: <%= from %>
-\nprTo: <%= to %>
+prAction: <%= action %>      
+pr标题: <%= title %>      
+pr地址: <%= url %>      
+pr状态: <%= state %>
+pr注释: <%= content %>
+pr提交人: <%= actionLogin %>
+pr提交时间: <%= createdAt %>
+pr更新时间: <%= updatedAt %>
+pr关闭时间: <%= closedAt %>
+pr合并时间: <%= mergedAt %>
+prFrom: <%= from %>
+prTo: <%= to %>
 `
     },
-    // github: '[github]\n\n'+
-    //         'repository:<%= repository.full_name %>\n\n'+
-    //         '<%= pusher.name %> pushed on branch <%= ref %> with the following commit(s):\n\n'+
-    //         '<% _.forEach(commits, function(c) { %>[<%= c.id.substr(0, 7) %>] <%= c.message %>\n\n<% }); %>'
+    // github: '[github]'+
+    //         'repository:<%= repository.full_name %>'+
+    //         '<%= pusher.name %> pushed on branch <%= ref %> with the following commit(s):'+
+    //         '<% _.forEach(commits, function(c) { %>[<%= c.id.substr(0, 7) %>] <%= c.message %><% }); %>'
   },
 };
