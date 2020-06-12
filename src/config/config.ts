8e144effa956f87ce7331a23fd1761c8d7e81772
w.c <52445078@qq.com>
2020-06-12 17:32:59 +0800

    re-format

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,7 +7,9 @@ export const config = {
       issue: (data: GithubWebHooks.WebhookPayloadIssues) => {
         const message = `
           [${data.repository.name}][issue#${data.issue.number}]
-          ${data.sender.login}: ${data.issue.title.length > 50 ? data.issue.title.substr(0, 50) + '...' : data.issue.title}
+          ${data.sender.login}(${data.action} issue)
+          
+          ${data.issue.title.length > 50 ? data.issue.title.substr(0, 50) + '...' : data.issue.title}
           ${data.issue.body.length > 50 ? data.issue.body.substr(0, 50) + '...' : data.issue.body}
 
           ${data.issue.html_url}
@@ -17,7 +19,9 @@ export const config = {
       issueComment: (data: GithubWebHooks.WebhookPayloadIssueComment) => {
         const message = `
           [${data.repository.name}][issue#${data.issue.number}]
-          ${data.sender.login}: ${data.issue.title.length > 50 ? data.issue.title.substr(0, 50) + '...' : data.issue.title}
+          ${data.sender.login}(new comment)
+          
+          ${data.issue.title.length > 50 ? data.issue.title.substr(0, 50) + '...' : data.issue.title}
           ${data.sender.login}: ${data.comment.body.length > 50 ? data.comment.body.substr(0, 50) + '...' : data.comment.body}
 
           ${data.comment.html_url}
